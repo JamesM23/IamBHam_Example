ecc30c7d121bf47ef0acb7206814020e1b351e34
JamesM23 <jhmay30@gmail.com>
2019-04-11 09:41:17 -0500

    leaders

--- litter-app-master/screens/Leaderboard.js
+++ litter-app-master/screens/Leaderboard.js
@@ -1,12 +1,23 @@
+import React, { Component } from 'react';
 import Leaderboard from 'react-native-leaderboard';
-//...
-this.state = {
+
+
+export default class Leaderboard extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
     data: [
         {userName: 'Joe', highScore: 52},
         {userName: 'Jenny', highScore: 120},
+        {userName: 'Billy', highScore: 45},
+        {userName: 'Riley', highScore: 64},
+        {userName: 'Pauly', highScore: 78},
+        {userName: 'Bobby', highScore: 100},
+        {userName: 'Earl', highScore: 20},
         //...
     ] //can also be an object of objects!: data: {a:{}, b:{}}
 }
+    }
  
 render() {
   return (
@@ -14,4 +25,6 @@ render() {
         data={this.state.data} 
         sortBy='highScore' 
         labelBy='userName'/>)
-  }
+
+}
+}
